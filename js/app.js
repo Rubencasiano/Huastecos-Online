7f836bd64a70cb64246c7c669f6ef543db9be9ca
Huastecos online <rubencasiano7@gmail.com>
2022-12-05 04:45:12 -0600

    Segundo Commit

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 //obtenemos la url del servidor
 var url=window.location.href;
 //definimos que nuestro sw.js se encuentra en el repositorio
-var ubicacionSw='/Huastecos-Online/sw.js';
+var ubicacionSw='Huastecos-Online/sw.js';
 if ( navigator.serviceWorker ) {
 /*Para que nuestro proyecto siga funcionando en localhost
 como en el servidor realizaremos una validación si la url
 contiene localhost la ruta es la local del proyecto, de lo contrario
 es la ruta de nuestro repositorio*/
 if(url.includes('Huastecos-Online')){
-ubicacionSw='sw.js';
+ubicacionSw='/sw.js';
 }
 navigator.serviceWorker.register(ubicacionSw);
 }
