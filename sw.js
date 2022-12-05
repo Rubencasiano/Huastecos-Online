6cf9dd75a050830e16e640a5f76072a4c052fd20
Huastecos online <rubencasiano7@gmail.com>
2022-12-05 02:13:16 -0600

    Segundo Commit

--- sw.js
+++ sw.js
@@ -7,6 +7,7 @@ self.addEventListener('install',evento=>{
     const promesa=caches.open(CACHE)
         .then(cache=>{
             return cache.addAll([
+                '/',
                 'about.html',
                 'offline.html',
                 'menu.html',
