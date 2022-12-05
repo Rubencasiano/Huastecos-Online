c9d3a12d12f1115d5b0a80e5b52ffab94f0cb93a
Huastecos online <rubencasiano7@gmail.com>
2022-12-05 02:17:06 -0600

    Segundo Commit

--- js/app.js
+++ js/app.js
@@ -1,10 +1,8 @@
-if ( navigator.serviceWorker ) {
-    navigator.serviceWorker.register('/sw.js');
-}
+//obtenemos la url del servidor
 var url=window.location.href;
 //definimos que nuestro sw.js se encuentra en el repositorio
 var ubicacionSw='/Ejemplo-PWA/sw.js';
-if ( navigator.serviceWorker ) 
+if ( navigator.serviceWorker ) {
 /*Para que nuestro proyecto siga funcionando en localhost
 como en el servidor realizaremos una validación si la url
 contiene localhost la ruta es la local del proyecto, de lo contrario
@@ -13,3 +11,4 @@ if(url.includes('localhost')){
 ubicacionSw='/sw.js';
 }
 navigator.serviceWorker.register(ubicacionSw);
+}
